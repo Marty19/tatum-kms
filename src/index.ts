90404ce1febb119f352fb42ae73f6bd3ee7db7fd
Marty19 <marty@ispace.co.uk>
2021-08-07 16:30:33 +0200

    Remove Password Checks

--- src/index.ts
+++ src/index.ts
@@ -112,11 +112,7 @@ const startup = async () => {
                     process.exit(-1);
                     return;
                 }
-            } else {
-                //pwd = question('Enter password to access wallet store:', {
-                //    hideEchoBack: true,
-                //});
-            }
+            } 
             process.env.TATUM_API_KEY = flags.apiKey;
             await processSignatures(pwd, flags.testnet, flags.period, flags.path, flags.chain?.split(','));
             break;
@@ -124,55 +120,32 @@ const startup = async () => {
             console.log(JSON.stringify(await generateWallet(command[1], flags.testnet), null, 2));
             break;
         case 'export':
-            exportWallets(question('Enter password to access wallet store:', {
-                hideEchoBack: true,
-            }), flags.path);
+            exportWallets('', flags.path);
             break;
         case 'generatemanagedwallet':
-            await storeWallet(command[1], flags.testnet,
-                question('Enter password to access wallet store:', {
-                    hideEchoBack: true,
-                }), flags.path);
+            await storeWallet(command[1], flags.testnet,'', flags.path);
             break;
         case 'storemanagedwallet':
-            await storeWallet(command[1], flags.testnet,
-                question('Enter password to access wallet store:', {
-                    hideEchoBack: true,
-                }), flags.path, question('Enter mnemonic to store:', {
+            await storeWallet(command[1], flags.testnet,'', flags.path, question('Enter mnemonic to store:', {
                     hideEchoBack: true,
                 }));
             break;
         case 'storemanagedprivatekey':
-            await storePrivateKey(command[1], flags.testnet,
-                question('Enter password to access wallet store:', {
-                    hideEchoBack: true,
-                }), question('Enter private key to store:', {
+            await storePrivateKey(command[1], flags.testnet,'', question('Enter private key to store:', {
                     hideEchoBack: true,
                 }), flags.path);
             break;
         case 'getmanagedwallet':
-            await getWallet(command[1],
-                question('Enter password to access wallet store:', {
-                    hideEchoBack: true,
-                }), flags.path);
+            await getWallet(command[1],'', flags.path);
             break;
         case 'getprivatekey':
-            await getPrivateKey(command[1], command[2],
-                question('Enter password to access wallet store:', {
-                    hideEchoBack: true,
-                }), flags.path);
+            await getPrivateKey(command[1], command[2],'', flags.path);
             break;
         case 'getaddress':
-            await getAddress(command[1], command[2],
-                question('Enter password to access wallet store:', {
-                    hideEchoBack: true,
-                }), flags.path);
+            await getAddress(command[1], command[2],'', flags.path);
             break;
         case 'removewallet':
-            await removeWallet(command[1],
-                question('Enter password to access wallet store:', {
-                    hideEchoBack: true,
-                }), flags.path);
+            await removeWallet(command[1],'', flags.path);
             break;
         default:
             console.error('Unsupported command. Use tatum-kms --help for details.');
